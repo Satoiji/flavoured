52a6e6317e1bf940402a2d1a386507656af6eab8
gustavo-soko <gustavo@sokolabs.com>
2019-11-25 18:56:17 -0700

    listen to port 80

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    port: process.env.PORT || 3000,
+    port: process.env.PORT || 8080,
     db: process.env.MONGOURI || "mongodb+srv://g_herrera:gatoperro@flavoured-classics-dmotk.mongodb.net/URM_collection?retryWrites=true&w=majority"
 }
