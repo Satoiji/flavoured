99b9f68511ce6f4d755ed84ef5b88e5f7c869d5d
Satoiji <ghch96biju@hotmail.com>
2019-12-01 02:27:31 -0700

    simple show profile

--- bot.js
+++ bot.js
@@ -544,15 +544,11 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                         break;
                         case PREFIX_PROFILE:
                             syntax = "--profile [@player]";
-                            bot.sendMessage({
-                                to: channelID,
-                                message: userID
-                            });
                             if(evt.d.mentions.length > 0){
                                 PLAYERS_MODEL.findOne({"discord_id": evt.d.mentions[0].id}, function(err, pl){
                                     if(!err){
                                     if(pl){
-                                        message += "- Discord: '" + pl.tag /*+ "' | Country: '" + pl.country.name */+ "' | elo: '" + pl.elo + "\n";
+                                        message = "- Discord: '" + pl.tag /*+ "' | Country: '" + pl.country.name */+ "' | elo: '" + pl.elo + "\n";
                                         bot.sendMessage({
                                             to: channelID,
                                             message: message
@@ -563,7 +559,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                                 PLAYERS_MODEL.findOne({"discord_id": userID}, function(err, pl){
                                     if(!err){
                                     if(pl){
-                                        message += "- Discord: '" + pl.tag /*+ "' | Country: '" + pl.country.name */+ "' | elo: '" + pl.elo + "\n";
+                                        message = "- Discord: '" + pl.tag /*+ "' | Country: '" + pl.country.name */+ "' | elo: '" + pl.elo + "\n";
                                         bot.sendMessage({
                                             to: channelID,
                                             message: message
